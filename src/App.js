962552299909e4301ea8d0d6f1cb2d03d7ac73d6
Vasanta Kumar <vasantakmr6@gmail.com>
2021-05-05 19:57:27 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -106,11 +106,12 @@ function App() {
             <div class="container">
               <div class="hero-inner">
                 <div class="hero-copy">
-                  <h1 class="hero-title mt-0">Tired of checking availability?</h1>
-                  <p class="hero-paragraph">Sit back and relax. Our script will check for availability every minute and let you know if its available.</p>
+                  <h1 class="hero-title mt-0">Tired of checking availability of vaccine?</h1>
+                  <p class="hero-paragraph">Sit back and relax. We will do it for you. We will check every minute and if there is a slot available you will get a notification by mail. Yup, it's that easy.</p>
                   <div class="hero-cta">
-                    <a class="button button-primary" href="#">Subscribe now</a>
-               
+                    <a class="button button-primary" target="_blank" rel="noopener noreferrer" href="https://github.com/vasantakmr/cowin-frontend"><svg class="github-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                    </svg><span class="p">Github Repo</span></a>
                   </div>
                 </div>
                 <div class="hero-media">
@@ -123,8 +124,8 @@ function App() {
                     <img class="hero-media-illustration-image asset-dark" src="images/hero-media-illustration-dark.svg" alt="Hero media illustration" />
                   </div>
                   <div class="hero-media-container">
-                    <img class="hero-media-image asset-light" src="images/hero-media-light.svg" alt="Hero media" />
-                    <img class="hero-media-image asset-dark" src="images/hero-media-dark.svg" alt="Hero media" />
+                    <img class="hero-media-image asset-light" src="images/hero-media-light.png" alt="Hero media" />
+                    <img class="hero-media-image asset-dark" src="images/hero-media-dark.png" alt="Hero media" />
                   </div>
                 </div>
               </div>
@@ -137,7 +138,7 @@ function App() {
                 <div class="features-header text-center">
                   <div class="container-sm">
                     <h2 class="section-title mt-0">How it Works?</h2>
-                    <p class="section-paragraph">Guide on how to make use of this website</p>
+                    <p class="section-paragraph">You just need to add your Pincode or select your state and district, and date. That's all. We will check availability for the next 7 days till you get your slot.</p>
                     <div class="features-image">
                       <img class="features-illustration asset-dark" src="images/features-illustration-dark.svg" alt="Feature illustration" />
                       <img class="features-box asset-dark" src="images/features-box-dark.svg" alt="Feature box" />
@@ -161,38 +162,36 @@ function App() {
                       </div>
                     </div>
                   </div>
-                  <div id='some-id'>
-                  </div>
-                  
-                  <div class="feature">
+
+                  <div class="feature ">
                     <div class="feature-inner">
+                      
                       <div class="feature-icon is-revealing">
                         <img class="asset-light" src="images/feature-02-light.svg" alt="Feature 02" />
                         <img class="asset-dark" src="images/feature-02-dark.svg" alt="Feature 02" />
                       </div>
-                      <div class="feature-content ">
-                        <h3 class="feature-title mt-0">Open-Source</h3>
-                        <p class="text-sm mb-0">Open-source is LOVE. Check our repo and contribute if possible</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="feature ">
-                    <div class="feature-inner">
-                      <div class="feature-icon  is-revealing">
-                        <img class="asset-light" src="images/feature-03-light.svg" alt="Feature 03" />
-                        <img class="asset-dark" src="images/feature-03-dark.svg" alt="Feature 03" />
-                      </div>
                       <div class="feature-content">
-                        <h3 class="feature-title mt-0">Check Availability and Subscribe</h3>
-                        <p class="text-sm mb-0"> Enter your PinCode or District along with the date to get notified if any spot is available</p>
+                        <h3 class="feature-title mt-0">How are we doing it?</h3>
+                        <p class="text-sm mb-0">Let's just say that we have a robot working 24 hours non stop for us and he is the real hero who is doing this for all of us.</p>
                       </div>
                     </div>
                   </div>
+                                    <div class="feature">
+                                      <div class="feature-inner">
+                                        <div class="feature-icon  is-revealing">
+                                          <img class="asset-light" src="images/feature-03-light.svg" alt="Feature 03" />
+                                          <img class="asset-dark" src="images/feature-03-dark.svg" alt="Feature 03" />
+                                        </div>
+                                        <div class="feature-content ">
+                                          <h3 class="feature-title mt-0">Open-Source</h3>
+                                          <p class="text-sm mb-0">Open-source is LOVE. Check our repo and contribute if possible</p>
+                                        </div>
+                                      </div>
+                                    </div>
                 </div>
               </div>
             </div>
           </section>
-
           
             <section class="section">
                   <div class="container-smm">
@@ -240,7 +239,7 @@ function App() {
                     <div class="cta-inner section-inner">
                         <div class="cta-header text-center">
                             <h2 class="section-title mt-0">Visit Cowin Portal and Register</h2>
-                            <p class="section-paragraph">Some paragraph.</p>
+                            <p class="section-paragraph">Cowin is a government website that allows us to register, book a slot for vaccination, get the vaccination certificate and much more. Pay a visit.</p>
 							<div class="cta-cta">
 								<a class="button button-primary" target="_blank" rel="noopener noreferrer" href="https://www.cowin.gov.in/">Open COWIN</a>
 							</div>
@@ -264,22 +263,20 @@ function App() {
                     </div>
                     <ul class="footer-links list-reset">
                         <li>
-                            <a href="#">Contact</a>
-                        </li>
-                        <li>
-                            <a href="#">About us</a>
-                        </li>
-                        <li>
-                            <a href="#">FAQ's</a>
-                        </li>
-                        <li>
-                            <a href="#">Support</a>
+                            <a href="mailto:cowin-availability@gmail.com">Contact</a>
                         </li>
                     </ul>
                     <ul class="footer-social-links list-reset">
-
+<li>
+    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/anujjha30/">
+        <span class="screen-reader-text">LinkedIn</span>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+<path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+</svg>
+    </a>
+</li>
                     </ul>
-                    <div class="footer-copyright">&copy; 2021 Vasant & Anuj, all rights reserved</div>
+                    <div class="footer-copyright">&copy;{" "}<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/vasantakmr">{" "}Vasant{" "}</a>  & {" "} <a  target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/anujjha30/">Anuj</a>, all rights reserved</div>
                 </div>
             </div>
         </footer>
@@ -553,7 +550,7 @@ function LoginForm(props) {
             className="form-select col-sm state"
             onChange={handleStateChange}
           >
-            <option value="⬇️ Select a State ⬇️"> -- Select a State -- </option>
+            <option value="⬇️ Select a State ⬇️">Select a State</option>
             {states.map((state) => (
               <option value={state.state_id}>{state.state_name}</option>
             ))}
@@ -565,8 +562,7 @@ function LoginForm(props) {
             onChange={handleDistrictChange}
           >
             <option value="⬇️ Select a District ⬇️">
-              {" "}
-              -- Select a District --{" "}
+              Select a District
             </option>
 
             {allDists.map((fruit) => (
